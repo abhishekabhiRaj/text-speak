16bd718f09408ffafd4703f8c2307a6875233745
abhishekabhiRaj <bcoder48@gmail.com>
2023-04-25 11:16:08 +0530

    by B Coder

---
+++ app.js
@@ -0,0 +1,18 @@
+  // Initialize the Web Speech API
+  const speechSynthesis = window.speechSynthesis;
+
+  function speak() {
+    // Input that you wrote
+    var input = document.getElementById('input-text').value
+    
+    // Create a new speech synthesis object
+    const readLine = new SpeechSynthesisUtterance(input);
+
+    // Set the voice
+    const voices = speechSynthesis.getVoices();
+    console.log("voices",voices)
+    readLine.voice = voices[1];
+
+    // Speak the text
+    speechSynthesis.speak(readLine);
+  }
